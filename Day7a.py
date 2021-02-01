29b4b8a0b5f6224cbc15545eecada5a9e2b3b437
Tzvi Friedman <tzvi.friedman@flatiron.com>
2021-02-01 14:16:00 -0500

    Tzvi was here

--- Day7a.py
+++ Day7a.py
@@ -3,11 +3,11 @@ import pprint
 count = 0
 
 
-with open(r'C:\Users\sfrie\Python\AdventofCode\Day7.txt') as file:
+with open(r'Day7.txt') as file:
     list_ = file.readlines()
     list_ = [y.strip() for y in list_]
     new_line = [re.split(r"contain|\,", line) for line in list_]
-    
+
 
 
 
@@ -20,41 +20,53 @@ for line in new_line:
     line =  [x.rsplit(' ', 1) for x in line]
     line = [x[:-1] for x in line]
     line = [y.strip(' ') for x in line for y in x]
-    
+
     diction[bag_key] = line[1:]
 
 
 pprint.pprint(diction)
-        
+
         #val, color = x.split(' ', 1)
-        
+
 
 
 def search_bag(current_bag):
     global count
     global bag
     global rules
+    print("current root is " + current_bag)
     rules = diction[current_bag]
+    print("It's children are " , rules)
     for x in rules:
         val, color = x.split(' ',1)
         if color == "shiny gold":
             count +=1
             return 'none'
-        elif color == 'other':
-            return "none"
+        # elif color == 'other':
+        #     current_bag = color
         else:
-            current_bag = color
-            return search_bag(current_bag)         
+            for k in diction.keys():    
+                if color in diction.keys():
+                    next_item = diction[color]
+                    if next_item[0] != 'no other':
+                        current_bag = color
+                        return search_bag(current_bag)
+                    # else:
+                    #     print(color, " contains no other bags")
+number = 0
+for bag in diction.keys():
     
-for bag, rules in diction.items():
-    for x in rules:
-        val, color = x.split(' ',1)
-        if color == 'other':
-            continue
-        else:
-            search_bag(color)
-    
-print(' total', count)
+        #val, color = x.split(' ',1)
+        #if color == 'other':
+        # continue
+    search_bag(bag)
+    number += 1
 
-        
-        
+print(' total', count, number)
+
+
+# 
+#
+#
+#
+#
