f5baa5564eb19983394f48dbfa160bd36ea833f6
shuaTW <shua@triplewhale.com>
2024-12-08 09:04:04 +0200

    3a

--- 2024/3.py
+++ 2024/3.py
@@ -1,7 +1,7 @@
 with open('2024/3.txt', 'r') as f:
     f = f.readlines()
 
-def maybe_in_mul(char):
+def maybe_in_mul(char, line_length):
     if char not in ['m', 'u', 'l', '(']:
         return False
     if char == 'm':
@@ -21,68 +21,54 @@ def calculate_mul(mul):
     mul = ''.join(mul)
     x, y = map(int, mul.split(','))
     return x*y
+
+def look_and_do_mul(
+    mul_started, in_mul, count, current_mul, inside_mul
+):
+    length_of_current_mul = len(current_mul)
+    if in_mul == True:
+        if char == ')':
+            mul = calculate_mul(inside_mul)
+            count+=mul
+            current_mul= []
+            inside_mul = []
+            in_mul = mul_started = False
+        elif char.isdigit() or char == ',':
+            inside_mul.append(char)
+        else:
+            current_mul= []
+            inside_mul = []
+            in_mul = mul_started = False
+        
+    elif mul_started:
+        if in_mul == False:
+            if char == '(' and length_of_current_mul == 3:
+                in_mul = True
+            elif mul_started == maybe_in_mul(char, length_of_current_mul):
+                if mul_started == True:
+                    current_mul.append(char)
+            else:
+                current_mul = []
+                inside_mul = []
+                in_mul = mul_started = False       
+
+    if mul_started == False:
+        mul_started = maybe_in_mul(char, length_of_current_mul)
+        if mul_started == True:
+            current_mul.append(char)
+    
+    return mul_started, in_mul, count, current_mul, inside_mul
     
 mul_started = False
 in_mul = False
 count = 0
 for line in f:
     current_mul = []
-    inside_mull = []
+    inside_mul = []
     for char in line:
-        line_length = len(current_mul)
-        
-        if in_mul == True:
-            if char == ')':
-                mul = calculate_mul(inside_mull)
-                count+=mul
-                current_mul= []
-                inside_mull = []
-                in_mul = mul_started = False
-            elif char.isdigit() or char == ',':
-                inside_mull.append(char)
-            else:
-                current_mul= []
-                inside_mull = []
-                in_mul = mul_started = False
-            
-        elif mul_started:
-            if in_mul == False:
-                if char == '(' and len(current_mul) == 3:
-                    in_mul = True
-                elif mul_started == maybe_in_mul(char):
-                    if mul_started == True:
-                        current_mul.append(char)
-                else:
-                    current_mul = []
-                    inside_mull = []
-                    in_mul = mul_started = False       
-
-        if mul_started == False:
-            mul_started = maybe_in_mul(char)
-            if mul_started == True:
-                current_mul.append(char)
-                
-        
-        # if in_mul == False:
-        #     mul_started = maybe_in_mul(char)
-        #     if mul_started:
-        #         current_mul.append(char)
-                
-        # if mul_started and len(current_mul) == 4:
-        #     in_mul = True
-        #     mul_started = False
-        # else:
-        #     current_mul = inside_mull = []
-        #     in_mul = mul_started = False 
-            
-        # if in_mul == True and char != ')':
-        #     inside_mull.append(char)
-        # elif in_mul == True and char == ')':
-        #     mul = calculate_mul(inside_mull)
-        #     count+=mul
-            
-        #     current_mul = inside_mull = []
-        #     in_mul = mul_started = False         
+        mul_started, in_mul, count, current_mul, inside_mul = look_and_do_mul(
+            mul_started, in_mul, count, current_mul, inside_mul
+        )               
            
 print(count)     
         
